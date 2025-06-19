a3c73d2d69b99b2dbbd851881ba3a1e848eb7b6b
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 00:10:47 +0530

    Updated code files: app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/contexts/ThemeContext"; // Added import
+
+import DashboardPage from "./pages/DashboardPage";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import PasswordRecoveryPage from "./pages/PasswordRecoveryPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> {/* Wrapped with ThemeProvider */}
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/password-recovery" element={<PasswordRecoveryPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
